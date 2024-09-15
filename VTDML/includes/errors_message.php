b18876ee78e68d5e274ac4f98af9df27c3c20fe1
vr0umvr0um <vanessa.antivackis@gmail.com>
2024-09-15 13:41:45 +0200

    Delete VTDML directory

--- VTDML/includes/errors_message.php
+++
@@ -1,9 +0,0 @@
-<?php if (count($errors) > 0): ?>
-    <div class="alert__message error">
-        <?php foreach ($errors as $errors): ?>
-            <p>
-                <?php echo $errors; ?>
-            </p>
-        <?php endforeach; ?>
-    </div>
-<?php endif; ?>
--- VTDML/includes/success_message.php
+++
@@ -1,9 +0,0 @@
-<?php if (isset($_SESSION['message'])): ?>
-<div class="alert__message success message">
-    <p>
-        <?php echo $_SESSION['message'] ?>
-        <?php unset($_SESSION['message'])?>
-    </p>
-</div>
-<?php else: ?>
-<?php endif; ?>
